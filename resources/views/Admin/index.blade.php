c1afd8aa3e840b969320aefdc4814bc18d404d82
mohamadkutainy <mohamadkutainy@example.com>
2023-12-03 19:05:31 +0300

    EndProjectLaravel

---
+++ resources/views/Admin/index.blade.php
@@ -0,0 +1,6 @@
+@extends('Admin.layout')
+
+@section('content')
+<h2 class="ml-50">hello from home page Admin</h2>
+
+@endsection
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">Home</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/users">users</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/posts">posts</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+@yield('cont')
+</body>
+</html>
